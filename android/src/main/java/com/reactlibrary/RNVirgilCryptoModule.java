f147cc2558dbb47a6ed1a4fda32df5fc1f0bc165
Vadim Avdeev <v.avdeiev@gmail.com>
2019-07-26 19:28:44 +0300

    setup Android

--- android/src/main/java/com/reactlibrary/RNVirgilCryptoModule.java
+++ android/src/main/java/com/reactlibrary/RNVirgilCryptoModule.java
@@ -5,14 +5,22 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.virgilsecurity.sdk.crypto.VirgilCrypto;
+
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+
 
 public class RNVirgilCryptoModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
+  private final VirgilCrypto crypto;
 
   public RNVirgilCryptoModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    this.crypto = new VirgilCrypto();
   }
 
   @Override
@@ -22,6 +30,11 @@ public class RNVirgilCryptoModule extends ReactContextBaseJavaModule {
 
   @ReactMethod(isBlockingSynchronousMethod = true)
   public String computeHash(String data) {
-    return data;
+    try {
+      byte[] hash = this.crypto.computeHash(data.getBytes("UTF-8"));
+      return Base64.encodeToString(hash, Base64.DEFAULT);
+    } catch (UnsupportedEncodingException e) {
+      return "";
+    }
   }
 }

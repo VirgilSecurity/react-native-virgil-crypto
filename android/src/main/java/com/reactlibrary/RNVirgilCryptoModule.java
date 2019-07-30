8c52727c2c446b3a280b821aacb8bd603b358563
Vadim Avdeev <v.avdeiev@gmail.com>
2019-07-30 19:59:00 +0300

    add generateKeyPair for Android

--- android/src/main/java/com/reactlibrary/RNVirgilCryptoModule.java
+++ android/src/main/java/com/reactlibrary/RNVirgilCryptoModule.java
@@ -1,15 +1,21 @@
 
 package com.reactlibrary;
 
+import android.util.Base64;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+
 import com.virgilsecurity.sdk.crypto.VirgilCrypto;
+import com.virgilsecurity.sdk.crypto.VirgilKeyPair;
 
-import android.util.Base64;
-
-import java.io.UnsupportedEncodingException;
+import com.reactlibrary.utils.ResponseFactory;
+import com.virgilsecurity.sdk.crypto.exceptions.CryptoException;
 
 
 public class RNVirgilCryptoModule extends ReactContextBaseJavaModule {
@@ -29,12 +35,25 @@ public class RNVirgilCryptoModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod(isBlockingSynchronousMethod = true)
-  public String computeHash(String data) {
+  public WritableMap computeHash(String data) {
+    byte[] hash = this.crypto.computeHash(data.getBytes(StandardCharsets.UTF_8));
+    return ResponseFactory.createStringResponse(Base64.encodeToString(hash, Base64.DEFAULT));
+  }
+
+  @ReactMethod(isBlockingSynchronousMethod = true)
+  public WritableMap generateKeyPair() {
     try {
-      byte[] hash = this.crypto.computeHash(data.getBytes("UTF-8"));
-      return Base64.encodeToString(hash, Base64.DEFAULT);
-    } catch (UnsupportedEncodingException e) {
-      return "";
+      VirgilKeyPair keypair = this.crypto.generateKeyPair();
+      final byte[] privateKeyData = this.crypto.exportPrivateKey(keypair.getPrivateKey());
+      final byte[] publicKeyData = this.crypto.exportPublicKey(keypair.getPublicKey());
+      Map<String, String> keypairMap = new HashMap<>();
+      keypairMap.put("privateKey", Base64.encodeToString(privateKeyData, Base64.DEFAULT));
+      keypairMap.put("publicKey", Base64.encodeToString(publicKeyData, Base64.DEFAULT));
+      return ResponseFactory.createMapResponse(keypairMap);
     }
+    catch (CryptoException e) {
+      return ResponseFactory.createErrorResponse(e);
+    }
+
   }
 }
---
+++ android/src/main/java/com/reactlibrary/utils/ResponseFactory.java
@@ -0,0 +1,90 @@
+package com.reactlibrary.utils;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
+
+import java.util.Map;
+import java.util.Iterator;
+
+public final class ResponseFactory {
+    private static final int ERROR_STACK_FRAME_LIMIT = 50;
+    private static final String ERROR_DEFAULT_CODE = "EUNSPECIFIED";
+    private static final String ERROR_DEFAULT_MESSAGE = "Error not specified.";
+
+    // Keys for error's WritableMap
+    private static final String ERROR_MAP_KEY_CODE = "code";
+    private static final String ERROR_MAP_KEY_MESSAGE = "message";
+    private static final String ERROR_MAP_KEY_USER_INFO = "userInfo";
+    private static final String ERROR_MAP_KEY_NATIVE_STACK = "nativeStackAndroid";
+
+    // Keys for ERROR_MAP_KEY_NATIVE_STACK's StackFrame maps
+    private static final String STACK_FRAME_KEY_CLASS = "class";
+    private static final String STACK_FRAME_KEY_FILE = "file";
+    private static final String STACK_FRAME_KEY_LINE_NUMBER = "lineNumber";
+    private static final String STACK_FRAME_KEY_METHOD_NAME = "methodName";
+
+
+    private static final WritableMap createParametersMap(Map<String, String> params) {
+        WritableMap paramsMap = Arguments.createMap();
+
+        if (!params.isEmpty()) {
+
+            Iterator<String> iterator = params.keySet().iterator();
+
+            while(iterator.hasNext()) {
+                String key = iterator.next();
+                paramsMap.putString(key, params.get(key));
+            }
+        }
+
+        return paramsMap;
+    }
+
+    private static final WritableMap createErrorInfoMap(Throwable throwable) {
+        WritableNativeMap errorInfo = new WritableNativeMap();
+        errorInfo.putString(ERROR_MAP_KEY_CODE, ERROR_DEFAULT_CODE);
+        errorInfo.putString(ERROR_MAP_KEY_MESSAGE, throwable.getMessage());
+        // For consistency with iOS ensure userInfo key exists, even if we null it.
+        // iOS: /React/Base/RCTUtils.m -> RCTJSErrorFromCodeMessageAndNSError
+        errorInfo.putNull(ERROR_MAP_KEY_USER_INFO);
+
+        StackTraceElement[] stackTrace = throwable.getStackTrace();
+        WritableNativeArray nativeStackAndroid = new WritableNativeArray();
+
+        // Build an an Array of StackFrames to match JavaScript:
+        // iOS: /Libraries/Core/Devtools/parseErrorStack.js -> StackFrame
+        for (int i = 0; i < stackTrace.length && i < ERROR_STACK_FRAME_LIMIT; i++) {
+            StackTraceElement frame = stackTrace[i];
+            WritableMap frameMap = new WritableNativeMap();
+            // NOTE: no column number exists StackTraceElement
+            frameMap.putString(STACK_FRAME_KEY_CLASS, frame.getClassName());
+            frameMap.putString(STACK_FRAME_KEY_FILE, frame.getFileName());
+            frameMap.putInt(STACK_FRAME_KEY_LINE_NUMBER, frame.getLineNumber());
+            frameMap.putString(STACK_FRAME_KEY_METHOD_NAME, frame.getMethodName());
+            nativeStackAndroid.pushMap(frameMap);
+        }
+
+        errorInfo.putArray(ERROR_MAP_KEY_NATIVE_STACK, nativeStackAndroid);
+        return errorInfo;
+    }
+
+    public static final WritableMap createStringResponse(String result) {
+        WritableMap response = Arguments.createMap();
+        response.putString("result", result);
+        return response;
+    }
+
+    public static final WritableMap createMapResponse(Map<String, String> params) {
+        WritableMap response = Arguments.createMap();
+        response.putMap("result", createParametersMap(params));
+        return response;
+    }
+
+    public static final WritableMap createErrorResponse(Throwable throwable) {
+        WritableMap response = Arguments.createMap();
+        response.putMap("error", createErrorInfoMap(throwable));
+        return response;
+    }
+}

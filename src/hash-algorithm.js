3b41502b181a5c8ff376425f5450fd144e05f266
Vadim Avdeev <v.avdeiev@gmail.com>
2019-08-02 20:08:02 +0300

    add HashAlgorithm and KeyPairType enums

---
+++ src/hash-algorithm.js
@@ -0,0 +1,6 @@
+export const HashAlgoritm = Object.freeze({
+  SHA224: 'SHA224',
+  SHA256: 'SHA256',
+  SHA384: 'SHA384',
+  SHA512: 'SHA512'
+});
---
+++ src/key-pair-type.js
@@ -0,0 +1,8 @@
+export const KeyPairType = Object.freeze({
+  ED25519: 'ED25519',
+  CURVE25519: 'CURVE25519',
+  SECP256R1: 'SECP256R1',
+  RSA_2048: 'RSA2048',
+  RSA_4096: 'RSA4096',
+  RSA_8192: 'RSA8192'
+});

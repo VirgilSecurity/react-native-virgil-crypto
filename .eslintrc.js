5ad9118ca9dd400ce710e04f7cf892b2fbd78b0d
Vadim Avdeev <v.avdeiev@gmail.com>
2019-08-02 20:06:17 +0300

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
